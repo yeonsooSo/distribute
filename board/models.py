570b67591e2793150f7743215035910c8c40de86
sanvit <sanvit@sanvit.me>
2021-05-10 21:25:42 +0000

    add blog model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TimeStampModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
+class Blog(TimeStampModel):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    image = models.ImageField(upload_to="blog/images/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
