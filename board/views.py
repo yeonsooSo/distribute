9bff2f34bcd754314e1af351b8a088ba73fd306f
sanvit <sanvit@sanvit.me>
2021-05-18 17:59:45 +0000

    add CRUD views

--- board/views.py
+++ board/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Blog, TimeStampModel
 # Create your views here.
+
+
+def home(req):
+    blog_objects = Blog.objects.all()
+    return render(req, 'home.html', {'data': blog_objects})
+
+
+def post_create(req):
+    if req.method == 'POST':
+        blog_object = Blog()
+        blog_object.title = req.POST['title']
+        blog_object.body = req.POST['body']
+        blog_object.save()
+        return redirect('/blog/'+str(blog_object.id))
+    return render(req, 'post_create.html')
+
+
+def post_read(req, id):
+    blog_object = get_object_or_404(Blog, pk=id)
+    return render(req, 'detail.html', {'data': blog_object})
+
+
+def post_update(req, id):
+    blog_object = get_object_or_404(Blog, pk=id)
+    if req.method == 'POST':
+        blog_object.title = req.POST['title']
+        blog_object.body = req.POST['body']
+        blog_object.save()
+        return redirect('/blog/'+str(blog_object.id))
+    return render(req, 'post_update.html')
+
+
+def post_delete(req, id):
+    blog_object = get_object_or_404(Blog, pk=id)
+    blog_object.delete()
+    return redirect('/')
